f0b4a605735ee6cdd85246515a2d364020f3564a
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-01-30 02:58:39 -0300

    feat: logout button

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -3,6 +3,8 @@ import { Component, computed, Input, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../auth/auth.service';
 
 export type MenuItem = {
   icon: string;
@@ -15,12 +17,15 @@ export type MenuItem = {
   imports: [
     MatListModule,
     MatIconModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SideNavComponent {
+  constructor(private authService: AuthService) {}
+
   sideNavCollapsed = signal(false);
   @Input() set collapsed(val: boolean) {
     this.sideNavCollapsed.set(val);
@@ -34,15 +39,19 @@ export class SideNavComponent {
     },
     {
       icon: 'person',
-      label: 'Pessoa',
+      label: 'Pessoas',
       route: 'person'
     },
     {
       icon: 'directions_car',
-      label: 'Veiculo',
+      label: 'Veiculos',
       route: 'vehicle'
     }
   ]);
+
+  handleLogout = () => {
+    this.authService.logout();
+  }
 
   profilePicSize = computed(() => this.sideNavCollapsed() ? '32' : '100');
 }
