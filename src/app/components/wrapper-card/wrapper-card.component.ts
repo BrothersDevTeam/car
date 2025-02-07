fe98828849d68fd8ce773c893acf09fb4dfb9460
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-02-07 10:09:27 -0300

    feat: wrapper-card

---
+++ src/app/components/wrapper-card/wrapper-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper-card',
+  imports: [],
+  templateUrl: './wrapper-card.component.html',
+  styleUrl: './wrapper-card.component.scss'
+})
+export class WrapperCardComponent {
+  @Input() title: string = '';
+}
