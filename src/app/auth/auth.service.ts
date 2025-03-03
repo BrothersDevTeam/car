b0bca75901c9f5601a1764f1afd8f62d19bfb467
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-03-03 17:24:44 -0300

    feat: proxyConfig and vercel.json for permite http://

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,18 +6,21 @@ import { tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  apiUrl: string = "http://controleautorevenda.duckdns.org/api/v1";
+  // apiUrl: string = "http://controleautorevenda.duckdns.org/api/v1";
+  apiUrl: string = '/api/v1';
 
   constructor(private httpClient: HttpClient, private router: Router) {}
 
   login(email: string, password: string) {
     return this.httpClient
-      .post<LoginResponse>(this.apiUrl + "/auth/login", { email, password })
-      .pipe(tap((value) => {
-        sessionStorage.setItem("car-token", value.token);
-        sessionStorage.setItem("username", value.fullName);
-        this.router.navigate(['/home']);
-      }));
+      .post<LoginResponse>(this.apiUrl + '/auth/login', { email, password })
+      .pipe(
+        tap((value) => {
+          sessionStorage.setItem('car-token', value.token);
+          sessionStorage.setItem('username', value.fullName);
+          this.router.navigate(['/home']);
+        })
+      );
   }
 
   logout() {
