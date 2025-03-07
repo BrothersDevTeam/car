b638693c16ffe9c08ebf5105dcf8abfcde586393
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-03-07 19:29:19 -0300

    feat: LegalEntity info

--- src/app/info/legal-entity-info/legal-entity-info.component.ts
+++ src/app/info/legal-entity-info/legal-entity-info.component.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 import { EventType } from '@angular/router';
+import { WrapperCardComponent } from '@components/wrapper-card/wrapper-card.component';
 import { GenericClient } from '@interfaces/entity';
 
 @Component({
   selector: 'app-legal-entity-info',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, WrapperCardComponent],
   templateUrl: './legal-entity-info.component.html',
   styleUrl: './legal-entity-info.component.scss',
 })
