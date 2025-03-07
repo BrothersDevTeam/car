39da53198f0c73ec7d7dd8ff9ef95e02d444b279
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-03-07 19:23:21 -0300

    feat: NaturalPerson info

--- src/app/info/natural-person-info/natural-person-info.component.ts
+++ src/app/info/natural-person-info/natural-person-info.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { EventType } from '@angular/router';
+import { WrapperCardComponent } from '@components/wrapper-card/wrapper-card.component';
 import { GenericClient } from '@interfaces/entity';
 
 @Component({
   selector: 'app-natural-person-info',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, WrapperCardComponent],
   templateUrl: './natural-person-info.component.html',
   styleUrl: './natural-person-info.component.scss',
 })
