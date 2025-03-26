87b43b8d76a01a75644350a24e95b373ce59016b
CarlosOtrebla <carlosotrebla@gmail.com>
2025-03-26 03:31:42 -0300

    Fix deletion confirmation messages

--- src/app/info/natural-person-info/natural-person-info.component.ts
+++ src/app/info/natural-person-info/natural-person-info.component.ts
@@ -1,14 +1,13 @@
 import { ToastrService } from 'ngx-toastr';
 import { EventType } from '@angular/router';
+import { Person } from '@interfaces/person';
 import { MatCardModule } from '@angular/material/card';
 import { PersonService } from '@services/person.service';
-import { GenericClient, Person } from '@interfaces/person';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogComponent } from '@components/dialog/dialog.component';
-import { Component, EventEmitter, inject, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { WrapperCardComponent } from '@components/wrapper-card/wrapper-card.component';
-import { PersonTableComponent } from "../../components/tables/person-table/person-table.component";
 
 @Component({
   selector: 'app-natural-person-info',
@@ -20,13 +19,9 @@ export class NaturalPersonInfoComponent {
 
   readonly dialog = inject(MatDialog);
 
-
-
-  // @Input() dataForm: GenericClient | null = null;
   @Input() person!: Person;
-  // @Input() selectedPerson!: Person;
   @Output() editEvent = new EventEmitter<EventType>();
-  @Output() formSubmitted = new EventEmitter<void>(); //confirmar essa linha
+  @Output() formSubmitted = new EventEmitter<void>();
 
   constructor(private toastrService: ToastrService, private personService: PersonService) { }
 
@@ -34,15 +29,13 @@ export class NaturalPersonInfoComponent {
     this.openDialog();
   }
 
-
-
   openDialog() {
     const dialogRef: MatDialogRef<DialogComponent> = this.dialog.open(
       DialogComponent,
       {
         data: {
-          title: 'Confirmar Deleção',
-          message: 'Você tem certeza que deseja deletar este registro?',
+          title: 'Confirmar Exclusão',
+          message: 'Você tem certeza que deseja excluir este registro?',
           confirmText: 'Sim',
           cancelText: 'Não',
         },
@@ -56,24 +49,23 @@ export class NaturalPersonInfoComponent {
     });
   }
 
-
   deleteConfirmed() {
     if (this.person.id) {
 
       this.personService.delete(this.person.id).subscribe({
         next: (response) => {
-          console.log('Deleção bem-sucedida', response);
-          this.toastrService.success('Deleção bem-sucedida');
+          console.log('Exclusão bem-sucedida', response);
+          this.toastrService.success('Exclusão bem-sucedida');
           this.formSubmitted.emit();
         },
         error: (error) => {
-          console.error('Erro ao deletar cliente', error);
-          this.toastrService.error('Erro ao deletar cliente');
+          console.error('Erro ao excluir cliente', error);
+          this.toastrService.error('Erro ao excluir cliente');
         },
       });
     } else {
-      console.error('ID não encontrado para deleção');
-      this.toastrService.error('ID não encontrado para deleção');
+      console.error('ID não encontrado para exclusão');
+      this.toastrService.error('ID não encontrado para exclusão');
     }
   }
 }
