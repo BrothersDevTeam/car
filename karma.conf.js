736033041f560e001a943b07a94b63bf2ca3a198
Cristiano Lopes Duarte <tkdcris@gmail.com>
2025-05-01 18:41:43 -0300

    feat: karma initial config

---
+++ karma.conf.js
@@ -0,0 +1,8 @@
+module.exports = function (config) {
+  config.set({
+    // Outras configurações...
+    client: {
+      captureConsole: true, // Captura os logs do console do navegador
+    },
+  });
+};
